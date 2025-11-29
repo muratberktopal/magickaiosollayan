a48219dc782b37cb301b3d864c73bccddcd9a7e3
ahmet salih demir <ahmetsalihdemir@hotmail.com>
2025-11-29 14:01:13 +0300

    asdfasdf
    asdfasdfasdf

--- Assets/scripts/MainMenuManager.cs
+++ Assets/scripts/MainMenuManager.cs
@@ -4,13 +4,27 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     [Header("Paneller")]
-    public GameObject modeSelectionPanel;
+    public GameObject modeSelectionPanel; // Survival/BR seçtiðin pencere
 
-    // ... (Ses kodlarýn burada kalabilir, dokunma) ...
+    [Header("Buton Gruplarý")]
+    public GameObject mainMenuButtons; // Play, Settings ve Quit butonlarýný içine koyacaðýn obje
 
     public void OpenModeSelection()
     {
+        // Mod seçimini aç, ana butonlarý gizle
         modeSelectionPanel.SetActive(true);
+
+        if (mainMenuButtons != null)
+            mainMenuButtons.SetActive(false);
+    }
+
+    public void CloseModeSelection()
+    {
+        // Mod seçimini kapat, ana butonlarý geri getir
+        modeSelectionPanel.SetActive(false);
+
+        if (mainMenuButtons != null)
+            mainMenuButtons.SetActive(true);
     }
 
     public void PlayBattleRoyale()
@@ -25,10 +39,5 @@ public class MainMenuManager : MonoBehaviour
         // Hafýzaya 1 yaz (1 = Survival)
         PlayerPrefs.SetInt("GameMode", 1);
         SceneManager.LoadScene(1); // Oyuna Gir
-    }
-
-    public void CloseModeSelection()
-    {
-        modeSelectionPanel.SetActive(false);
     }
 }
